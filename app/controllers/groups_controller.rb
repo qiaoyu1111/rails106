e4e2c0e5833e8ba33bbecac50fbb841c258cf239
qiaoyu <qiaoyu1111358@gmail.com>
2017-03-16 23:01:08 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,37 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.new(group_params)
+    if @group.edit
+      redirect_to groups_path, notice: "Update Success !"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "Group Deleted ."
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
