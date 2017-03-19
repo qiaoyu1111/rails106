3be27dd516181a99ac83405bc69b3b07fa449547
qiaoyu <qiaoyu1111358@gmail.com>
2017-03-19 17:58:45 +0800

    text

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :groups do
     member do
       post 'upvote'
       post 'devote'
     end
   end
+  
   root 'groups#index'
 end
