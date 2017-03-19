090ccadf5c5d99e3ded4f8d9669f29ccb75d9e88
qiaoyu <qiaoyu1111358@gmail.com>
2017-03-19 18:02:50 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :groups do
+    resources :posts
+  end
+
+  resources :groups do
     member do
       post 'upvote'
       post 'devote'
     end
   end
-  
+
   root 'groups#index'
 end
