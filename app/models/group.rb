f8a520659ee6f9ff24b02eff2f68047d04266a7f
qiaoyu <qiaoyu1111358@gmail.com>
2017-03-19 18:21:41 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,7 @@ class Group < ApplicationRecord
   has_many :posts
   validates :title, presence: true
   has_many :votes, dependent: :destroy
+
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
